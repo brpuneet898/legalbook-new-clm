6b8121f64ee9b159635ebb4edc0dae5d40aa5bf4
brpuneet898 <brpuneet898@gmail.com>
2025-09-17 22:33:15 +0530

    added step 1

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/ai-generate-draft')
+def ai_generate_draft():
+    return render_template('aidraft.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
